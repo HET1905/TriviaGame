715a687f5eb97266de35ed7b80e0a66706b5dfe9
Hetal Patel <nphetal1905@gmail.com>
2019-07-05 22:37:57 -0400

    update js file for image load

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,42 @@
 var num = 0;
 var correctAns;
-var numCorrectAns=0;
-var numWrongAns=0;
-var numUnattended=0;
+var numCorrectAns = 0;
+var numWrongAns = 0;
+var numUnattended = 0;
 var quseAnsObj = {
     "ques1": {
-        "q": " By what name was William F. Cody better known?",
-        "a": ["Buffalo Bill", "Horn Bill", "Horn", "Buffalo King"]
+        "q": " What’s the total number of dots on a pair of dice?",
+        "a": ["42", "32", "22", "30"],
+        "src": "assets/images/dice.jpg"
     },
     "ques2": {
         "q": "What is the capital city of Peru?",
-        "a": ["Swiden", "Sydney", "Barzil", "Lima"]
+        "a": ["Swiden", "Sydney", "Barzil", "Lima"],
+        "src": "assets/images/peru.jpg"
     },
-    "ques3":{
-        "q":"Which fictional city is the home of Batman?",
-        "a":["Duckberg","Gotham City","Metropolis","Central City"]
+    "ques3": {
+        "q": "Which fictional city is the home of Batman?",
+        "a": ["Duckberg", "Gotham City", "Metropolis", "Central City"],
+        "src": "assets/images/gotham.jpeg"
     },
-    "ques4":{
+    "ques4": {
         "q": "Spinach is high in which mineral?",
-        "a":["Calcium","Potassium","Iron","Zinc"]
+        "a": ["Calcium", "Potassium", "Iron", "Zinc"],
+        "src": "assets/images/spinach.jpg"
     },
-    "ques5":{
+    "ques5": {
         "q": "Which crime-fighting cartoon dog has the initals “S.D.” on his collar?",
-        "a":["Goofy","Scooby Doo","Pluto","Underdog"]
+        "a": ["Goofy", "Scooby Doo", "Pluto", "Underdog"],
+        "src": "assets/images/Scoobydoo.jpg"
     }
 
 }
 var rightAnsObj = {
-    "ques1": "Buffalo Bill",
+    "ques1": "42",
     "ques2": "Lima",
-    "ques3":"Gotham City",
-    "ques4":"Iron",
-    "ques5":"Scooby Doo"
+    "ques3": "Gotham City",
+    "ques4": "Iron",
+    "ques5": "Scooby Doo"
 
 }
 
@@ -41,9 +46,11 @@ $(document).ready(function () {
     var time = 11;
     var timerText = $("<span>");
     var timingLabel = $("<h2>");
+    // var img = "<img>"
     var intervalId;
 
     function createDynamicElements() {
+        // alert("inside create")
         num++;
         if (num < 6) {
             timingLabel.append("Timming Remaining : ");
@@ -56,11 +63,23 @@ $(document).ready(function () {
             $("#dynamicDiv").empty();
             // $("#dynamicDiv").append(timingLabel,timerText);
             $("#dynamicDiv").append("<h2>All done, heres hows you did!");
-            $("#dynamicDiv").append("<ul><li> Correc Answers : " + numCorrectAns + "</li>");
-            $("#dynamicDiv").append("<li> Incorrect Answers : " + numWrongAns + "</li>");
-            $("#dynamicDiv").append("<li> Unanswered : " + numUnattended + "</li></ul>");
+            $("#dynamicDiv").append("<ul id=ulDynamic><li class=listDynamic> Correc Answers : " + numCorrectAns + "</li>");
+            $("#dynamicDiv").append("<li class=listDynamic> Incorrect Answers : " + numWrongAns + "</li>");
+            $("#dynamicDiv").append("<li class=listDynamic> Unanswered : " + numUnattended + "</li></ul>");
+            $("#dynamicDiv").append("<button id=btnRestart>Start Over Again</button>");
         }
     }
+
+    $("#dynamicDiv").on("click", "#btnRestart", function () {
+        alert("From restart");
+        var num = 0;
+        var numCorrectAns = 0;
+        var numWrongAns = 0;
+        var numUnattended = 0;
+        $("#dynamicDiv").empty();
+        createDynamicElements();
+
+    });
 
     function counter() {
         if (!timerFlag) {
@@ -80,7 +99,6 @@ $(document).ready(function () {
             time = 11;
         }
     }
-
 
 
     function printQuestion(num) {
@@ -105,8 +123,6 @@ $(document).ready(function () {
             printAnswer(num);
         }
 
-
-
     }
 
 
@@ -121,7 +137,6 @@ $(document).ready(function () {
                 'id': liId,
                 'class': 'listDynamic'
             }).html(liValue).click(function () {
-                // alert($(this).text());
                 correctAns = $(this).text();
                 checkAnswer(qNo)
             });
@@ -129,44 +144,37 @@ $(document).ready(function () {
             $("#ulDynamic").append(li);
 
         }
-
     }
-
 
     function checkAnswer(num) {
         var quesNo = "ques" + num;
         if (correctAns === rightAnsObj[quesNo]) {
-            alert("Right Answer !!!");
+            // alert("Right Answer !!!");
             numCorrectAns++;
             $("#quesHeadingId").remove();
             $("#ulDynamic").remove();
-           
+
             $("#dynamicDiv").append("<h2 id=nope >Correct !!! </h2>");
             $("#dynamicDiv").append("<p id=correctAnswerP> Answer is : " + rightAnsObj[quesNo] + "</p>");
-
+            // $(img).attr('src',quseAnsObj[quesNo].src);
+            $("#dynamicDiv").append("<img src=" + quseAnsObj[quesNo].src + " id=ansImg />");
             emptyDynamicDivId = setTimeout(function () {
                 $("#nope").remove();
                 $("#correctAnswerP").remove();
+                $("#ansImg").remove();
             }, 2000);
-            var setTimeOut1 = setTimeout(createDynamicElements, 3000);
+            var setTimeOut1 = setTimeout(createDynamicElements, 2000);
 
             clearInterval(intervalId);
             timerFlag = false
             time = 11;
             timingLabel.text("");
             timerText.text("");
-            // createDynamicElements();
         } else {
-            alert("Wrong Answer");
-            // numWrongAns++;
+            // alert("Wrong Answer");
             correctAnswerDiv(num);
-            time=11;
-            // emptyDynamicDivId = setTimeout(function(){
-            //     $("#nope").remove();
-            //     $("#correctAnswerP").remove();
-            // },2000)
-            // createDynamicDivIntId = setTimeout(createDynamicElements(),2000);
-            // createDynamicElements();
+            time = 11;
+
         }
     }
 
@@ -177,7 +185,8 @@ $(document).ready(function () {
             $("#quesHeadingId").remove();
             $("#ulDynamic").remove();
             $("#dynamicDiv").append("<h2 id=nope >Running Out of time ! </h2>");
-            $("#dynamicDiv").append("<p id=correctAnswerP>The Correct answer is : " + rightAnsObj[quesNo] + "</p>");
+            $("#dynamicDiv").append("<p id=correctAnswerP>The Correct answer was : " + rightAnsObj[quesNo] + "</p>");
+            $("#dynamicDiv").append("<img src=" + quseAnsObj[quesNo].src + " id=ansImg />");
 
             timingLabel.text("");
             timerText.text("");
@@ -187,6 +196,7 @@ $(document).ready(function () {
             emptyDynamicDivId = setTimeout(function () {
                 $("#nope").remove();
                 $("#correctAnswerP").remove();
+                $("#ansImg").remove();
             }, 2000);
             var setTimeOut1 = setTimeout(createDynamicElements, 3000);
         } else {
@@ -194,17 +204,18 @@ $(document).ready(function () {
             $("#quesHeadingId").remove();
             $("#ulDynamic").remove();
             $("#dynamicDiv").append("<h2 id=nope >Nope ! </h2>");
-            $("#dynamicDiv").append("<p id=correctAnswerP>The Correct answer is : " + rightAnsObj[quesNo] + "</p>");
-
+            $("#dynamicDiv").append("<p id=correctAnswerP>The Correct answer was : " + rightAnsObj[quesNo] + "</p>");
+            $("#dynamicDiv").append("<img src=" + quseAnsObj[quesNo].src + " id=ansImg />");
             timingLabel.text("");
             timerText.text("");
             clearInterval(intervalId);
             timerFlag = false;
-               emptyDynamicDivId = setTimeout(function () {
+            emptyDynamicDivId = setTimeout(function () {
                 $("#nope").remove();
                 $("#correctAnswerP").remove();
+                $("#ansImg").remove();
             }, 2000);
-            var setTimeOut1 = setTimeout(createDynamicElements, 3000);
+            var setTimeOut1 = setTimeout(createDynamicElements, 2000);
         }
 
     }
@@ -212,11 +223,11 @@ $(document).ready(function () {
 
 
 
-$("#btnStart").click(function () {
-    $("#btnDiv").hide();
-    createDynamicElements();
+    $("#btnStart").click(function () {
+        $("#btnDiv").hide();
+        createDynamicElements();
 
-});
+    });
 
 
 });
